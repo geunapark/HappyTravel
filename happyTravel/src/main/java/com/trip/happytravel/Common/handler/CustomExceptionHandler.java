e6aa17ef851477cb2d7e7c2234af2ba5aba12e30
DESKTOP-VVUATKV\geuna <geuna_1019@naver.com>
2024-10-03 03:29:10 +0900

    메서드 오류는 아직 바디에 안뜸 하지만 409에러는 바디에 뜨게 생성함

--- happyTravel/src/main/java/com/trip/happytravel/Common/handler/CustomExceptionHandler.java
+++ happyTravel/src/main/java/com/trip/happytravel/Common/handler/CustomExceptionHandler.java
@@ -1,11 +1,16 @@
 package com.trip.happytravel.Common.handler;
 
+import ch.qos.logback.core.spi.ErrorCodes;
+import com.trip.happytravel.Common.errocode.ErrorCode;
+import com.trip.happytravel.Common.exception.CommonResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.DuplicateFormatFlagsException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,6 +29,18 @@ public class CustomExceptionHandler {
         }
 
         return ResponseEntity.badRequest().body(errorCode);
+    }
+
+
+    @ExceptionHandler(CommonResponse.class)
+    public ResponseEntity<Map<String, String>> handleCommonException(CommonResponse ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+
+        // 에러 코드와 메시지를 CommonException에서 가져오기
+        errorResponse.put("ErrorCode", String.valueOf(ex.getStatus()));
+        errorResponse.put("message", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(errorResponse);
     }
 
 
